01e336d6abde4fb59d630964001ed976b1321dca
Enzo Siqueira <100931687+enzosiqueira@users.noreply.github.com>
2023-10-24 11:53:40 -0300

    Add files via upload

--- Aula 10-10/script.js
+++ Aula 10-10/script.js
@@ -1,21 +1,21 @@
 // Criamos um objeto 'person' para armazenar os dados da pessoa.
 const person = {
-    // Inicializamos as propriedades do objeto com valores vazios.
-    firstName: "",
-    lastName: "",
-    language: "",
-    // Definimos um método 'setDetails' para atualizar as propriedades do objeto com os dados do formulário, funciona como um método construtor.
-    setDetails(firstName, lastName, language) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.language = language;
-    },
-    // Definimos um método 'displayDetails' para exibir os detalhes da pessoa no HTML.
-    displayDetails() {
-        document.getElementById("displayFirstName").textContent = this.firstName;
-        document.getElementById("displayLastName").textContent = this.lastName;
-        document.getElementById("displayLanguage").textContent = this.language;
-    }
+  // Inicializamos as propriedades do objeto com valores vazios.
+  firstName: "",
+  lastName: "",
+  age: "",
+  // Definimos um método 'setDetails' para atualizar as propriedades do objeto com os dados do formulário, funciona como um método construtor.
+  setDetails(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+  },
+  // Definimos um método 'displayDetails' para exibir os detalhes da pessoa no HTML.
+  displayDetails() {
+      document.getElementById("displayFirstName").textContent = this.firstName;
+      document.getElementById("displayLastName").textContent = this.lastName;
+      document.getElementById("displayAge").textContent = this.age;
+  }
 };
 
 // Obtemos uma referência ao formulário HTML com o ID "personForm".
@@ -23,36 +23,17 @@ const form = document.getElementById("personForm");
 
 // Adicionamos um evento de escuta para o evento "submit" do formulário.
 form.addEventListener("submit", function (e) {
-    // Impedimos o comportamento padrão de envio do formulário.
-    e.preventDefault();
+  // Impedimos o comportamento padrão de envio do formulário.
+  e.preventDefault();
 
-    // Obtemos os valores dos campos de entrada do formulário.
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const language = document.getElementById("language").value;
+  // Obtemos os valores dos campos de entrada do formulário.
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const age = document.getElementById("age").value;
 
-    // Chamamos o método 'setDetails' metodo construtor, neste momento estamos construindo o objeto criado
-    person.setDetails(firstName, lastName, language);
-    
-    // Chamamos o método 'displayDetails' do objeto 'person' para exibir os detalhes no HTML.
-    person.displayDetails();
+  // Chamamos o método 'setDetails' metodo construtor, neste momento estamos construindo o objeto criado
+  person.setDetails(firstName, lastName, age);
+  
+  // Chamamos o método 'displayDetails' do objeto 'person' para exibir os detalhes no HTML.
+  person.displayDetails();
 });
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
